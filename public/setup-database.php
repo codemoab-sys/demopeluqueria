59d8ea89b849ad89f75b83bab97beeb6358cebb8
codemoab-sys <codemoab-sys@users.noreply.github.com>
2026-08-02 22:15:13 -0500

    v

--- public/setup-database.php
+++ public/setup-database.php
@@ -26,3 +26,8 @@ echo $buffer;
 echo '</pre>';
 echo '<p><strong>Código de salida:</strong> ' . $status . '</p>';
 echo '</body></html>';
+
+CREATE DATABASE IF NOT EXISTS avicola1_bdavicola;
+CREATE USER IF NOT EXISTS 'avicola1_userbd'@'localhost' IDENTIFIED BY 'TU_PASSWORD_REAL';
+GRANT ALL PRIVILEGES ON avicola1_bdavicola.* TO 'avicola1_userbd'@'localhost';
+FLUSH PRIVILEGES;
